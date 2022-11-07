2f68ce1e965d0b5984b7f8aff6b60a00d356a200
shreyas0924 <shreyasp0924@gmail.com>
2022-11-07 19:27:25 +0530

    updated header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold mt-6 flex justify-center">Weather Forecast</h1>;
+  return (
+    <h1 className="text-3xl font-bold mt-6 flex justify-center">
+      Weather Forecast
+    </h1>
+  );
 }
 
 export default App;
