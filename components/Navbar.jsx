a1dd940e80c1ba4ad822a60c03370ad17cdf3a4f
SHUBHANKAR SHARMA <shubhankarsharma22@gmail.com>
2022-11-08 19:43:00 +0530

    updated the title with refresh action of the same page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <h1 className="text-[40px] font-bold mt-6 flex justify-center">
+      <a href="http://localhost:5173/"><h1 className="text-[40px] font-bold mt-6 flex justify-center">
         ⛈️ Weather Forecast ☀️
-      </h1>
+      </h1></a>
     </div>
   );
 };
