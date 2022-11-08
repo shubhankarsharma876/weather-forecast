2d8717d6a9f0505c212dc43c1973996baf253436
SHUBHANKAR SHARMA <shubhankarsharma22@gmail.com>
2022-11-08 22:51:20 +0530

    trial updates

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Weather = () => {
   return (
     <div>
-      
+      <div className="header text-center ...">CityName:</div>
+      <div className="middle flex gap-5 justify-center">
+        <div className="Day Date text-center ...">Date/Date:</div>
+        <div className="description text-center ...">Weather:</div>
+      </div>
+      <div className="lower flex gap-5 justify-center">
+        <div className="temperature text-center ...">Temperature:</div>
+        <div className="humidity text-center ...">Humidity:</div>
+      </div>
     </div>
   )
 }
