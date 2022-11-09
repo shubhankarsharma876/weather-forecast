b49e7f16b4cd9f7d5dabcaa782d596a7fc2af392
SHUBHANKAR SHARMA <shubhankarsharma22@gmail.com>
2022-11-09 20:06:46 +0530

    Weather details maintained in center

--- src/App.jsx
+++ src/App.jsx
@@ -34,30 +34,30 @@ function App() {
       </div>
       <div className="container">
         <div className="top">
-          <div className="location">
+          <div className="location text-center ">
             <p>{data.name}</p>
           </div>
-          <div className="temp">
+          <div className="temp text-center">
             {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
-          <div className="description">
+          <div className="description text-center">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
         {data.name !== undefined && (
           <div className="bottom">
-            <div className="feels">
+            <div className="feels text-center">
               {data.main ? (
                 <p className="bold">{data.main.feels_like.toFixed()}°F</p>
               ) : null}
               <p>Feels Like</p>
             </div>
-            <div className="humidity">
+            <div className="humidity text-center">
               {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
               <p>Humidity</p>
             </div>
-            <div className="wind">
+            <div className="wind text-center">
               {data.wind ? (
                 <p className="bold">{data.wind.speed.toFixed()} MPH</p>
               ) : null}
