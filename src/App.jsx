ccec69e16c568a1a139afdaf5a85fbd34206f0fe
shreyas0924 <shreyasp0924@gmail.com>
2022-11-09 20:12:18 +0530

    pr merged

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
@@ -33,16 +33,6 @@ function App() {
         />
       </div>
       <div className="container">
-<<<<<<< HEAD
-        <div className="top text-xl">
-          <div className="location text-xl">
-            <p>{data.name}</p>
-          </div>
-          <div className="temp text-xl">
-            {data.main ? <h1>{data.main.temp}°C</h1> : null}
-          </div>
-          <div className="description text-xl">
-=======
         <div className="top">
           <div className="location text-center ">
             <p>{data.name}</p>
@@ -51,39 +41,25 @@ function App() {
             {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
           <div className="description text-center">
->>>>>>> 3bd9892a3dcb3b13091829d3e262fa09e57cb032
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
         {data.name !== undefined && (
-<<<<<<< HEAD
-          <div className="bottom text-xl">
-            <div className="feels text-xl">
-=======
           <div className="bottom">
             <div className="feels text-center">
->>>>>>> 3bd9892a3dcb3b13091829d3e262fa09e57cb032
               {data.main ? (
-                <p className="bold text-xl">{data.main.feels_like.toFixed()}°C</p>
+                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
               ) : null}
               <p>Feels Like</p>
             </div>
-<<<<<<< HEAD
-            <div className="humidity text-xl">
-              {data.main ? <p className="bold text-xl">{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
-            </div>
-            <div className="wind text-xl">
-=======
             <div className="humidity text-center">
               {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
               <p>Humidity</p>
             </div>
             <div className="wind text-center">
->>>>>>> 3bd9892a3dcb3b13091829d3e262fa09e57cb032
               {data.wind ? (
-                <p className="bold text-xl">{data.wind.speed.toFixed()} KPH</p>
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
               ) : null}
               <p>Wind Speed</p>
             </div>
