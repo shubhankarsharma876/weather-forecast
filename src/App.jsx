9433de66409407d5e4c55e6d2b2f27a8e9deb620
shreyas0924 <shreyasp0924@gmail.com>
2022-11-09 20:08:54 +0530

    updated metrcis

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
@@ -33,33 +33,33 @@ function App() {
         />
       </div>
       <div className="container">
-        <div className="top">
-          <div className="location">
+        <div className="top text-xl">
+          <div className="location text-xl">
             <p>{data.name}</p>
           </div>
-          <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          <div className="temp text-xl">
+            {data.main ? <h1>{data.main.temp}°C</h1> : null}
           </div>
-          <div className="description">
+          <div className="description text-xl">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
         {data.name !== undefined && (
-          <div className="bottom">
-            <div className="feels">
+          <div className="bottom text-xl">
+            <div className="feels text-xl">
               {data.main ? (
-                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+                <p className="bold text-xl">{data.main.feels_like.toFixed()}°C</p>
               ) : null}
               <p>Feels Like</p>
             </div>
-            <div className="humidity">
-              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+            <div className="humidity text-xl">
+              {data.main ? <p className="bold text-xl">{data.main.humidity}%</p> : null}
               <p>Humidity</p>
             </div>
-            <div className="wind">
+            <div className="wind text-xl">
               {data.wind ? (
-                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+                <p className="bold text-xl">{data.wind.speed.toFixed()} KPH</p>
               ) : null}
               <p>Wind Speed</p>
             </div>
