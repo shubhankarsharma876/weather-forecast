2c23d365ad3997e7dd0ef01c7600d9f137f33562
shreyas0924 <shreyasp0924@gmail.com>
2022-11-12 10:32:15 +0530

    seetup database

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 
-
+app.get("/", (req, res) => {
+  res.send("hello world i am a server");
+});
 
 app.listen(5174, () => {
-  console.log('Hi server')
-})
+  console.log("Running on port 5174");
+});
