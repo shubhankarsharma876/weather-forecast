48d0c5e868e0279659092c62d499cc1edee2ecf2
shreyas0924 <shreyasp0924@gmail.com>
2022-11-12 20:39:00 +0530

    connectted db

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "weatherdb",
+});
+
 app.get("/", (req, res) => {
   res.send("hello world i am a server");
 });
