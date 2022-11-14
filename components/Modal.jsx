d7e579616bbdb744a144559d433852bc8b47926e
shreyas0924 <shreyasp0924@gmail.com>
2022-11-14 19:27:41 +0530

    updated modal

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -2,17 +2,25 @@ import React, { useState } from "react";
 import "./Modal.css";
 
 export default function Modal() {
-  const [feedback, setFeedback] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  const [name, setName] = useState("");
+  const [feedbackdb, setFeedbackdb] = useState("");
+  const [email, setEmail] = useState("");
 
   const toggleModal = () => {
-    setFeedback(!feedback);
+    setModal(!modal);
   };
 
-  if (feedback) {
+  if (modal) {
     document.body.classList.add("active-modal");
   } else {
     document.body.classList.remove("active-modal");
   }
+
+  const displayInfo = () => {
+    console.log(name + " " + email + " " + feedbackdb);
+  };
 
   return (
     <>
@@ -23,7 +31,7 @@ export default function Modal() {
         Feedback
       </button>
 
-      {feedback && (
+      {modal && (
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content ">
@@ -35,18 +43,25 @@ export default function Modal() {
                   <input
                     type="text"
                     className="rounded-md w-[370px] ml-7 "
+                    onChange={(event) => setName(event.target.value)}
                   ></input>
                 </div>
+
                 <div className="flex gap-2 p-2  mt-5 text-xl  ">
                   <p className="justify-start">Email: </p>
                   <input
                     type="email"
                     className="rounded-md w-[370px] ml-9"
+                    onChange={(event) => setEmail(event.target.value)}
                   ></input>
                 </div>
                 <div className="flex gap-2 p-2  mt-5 text-xl ">
                   <p className="justify-start">Feedback: </p>
-                  <input type="text" className="rounded-md w-[370px]  "></input>
+                  <input
+                    type="text"
+                    className="rounded-md w-[370px]"
+                    onChange={(event) => setFeedbackdb(event.target.value)}
+                  ></input>
                 </div>
               </div>
             </div>
@@ -58,16 +73,15 @@ export default function Modal() {
               Close
             </button>
 
-
-            <button className="border-2 border-black rounded-xl mt-6 p-2 w-1/2 ml-[25%]">Submit</button>
+            <button
+              className="border-2 border-black rounded-xl mt-6 p-2 w-1/2 ml-[25%]"
+              onClick={displayInfo}
+            >
+              Submit
+            </button>
           </div>
-
-          
         </div>
-        
       )}
-
-      
     </>
   );
 }
